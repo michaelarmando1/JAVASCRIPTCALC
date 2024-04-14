949bc80346713f7b624b5ed65dae3a28ace38763
michaelamando1 <michaelamando25@gmail.com>
2024-04-14 21:32:19 +0100

    Simple Calculations

---
+++ calc.js
@@ -0,0 +1,24 @@
+// Addition
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(5,4))
+  
+  // Subtraction
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  // Multiplication
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  // Division
+  function divide(a, b) {
+    if (b === 0) {
+      return "Cannot divide by zero";
+    }
+    return a / b;
+  }
